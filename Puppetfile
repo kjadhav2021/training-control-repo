3becb4c83244c566c5e3222f6aaf3e1329c2f1e8
kjadhav2021 <kjadhav102015@gmail.com>
2021-06-13 02:11:16 +0800

    created accounts_baseline
    added local security module in Puppetfile
    created accounts_baseline

--- Puppetfile
+++ Puppetfile
@@ -18,3 +18,5 @@ forge 'https://forge.puppet.com'
 
 #puppet support task
 mod 'puppetlabs-support_tasks', '1.1.3'
+#local security policy
+mod 'ayohrling-local_security_policy', '0.8.1'
---
+++ site-modules/profile/manifests/accounts_baseline.pp
@@ -0,0 +1,28 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile::accounts_baseline
+class profile::accounts_baseline (
+  Hash[String, String] $users = { 'title' => 'Art Vandelay',
+  'name' => 'vandelay',
+  'groups' => "['BUILTIN\Administrators']" ,
+  'comment' => 'Vandelay Industries Administrator - managed by Puppet'},
+  String $local_policy = 'Log on as a service',
+  String $policy_value = '*S-1-5-80-0,vandelay',
+){
+  user{ $users['title']:
+      ensure     => present,
+      name       => $users['name'],
+      groups     => $users['groups'],
+      comment    => $users['comment'],
+      password   => 'puppetlabs',
+      managehome => true,
+  }
+
+  local_security_policy { $local_policy:
+    ensure       => 'present',
+    policy_value => $policy_value,
+  }
+}
