bc7ec4707cc94289176e1a567301a76f59ce2e70
kjadhav2021 <kjadhav102015@gmail.com>
2021-06-13 16:42:25 +0800

    update accounts_baseline.pp

--- site-modules/profile/manifests/accounts_baseline.pp
+++ site-modules/profile/manifests/accounts_baseline.pp
@@ -10,17 +10,29 @@ class profile::accounts_baseline (
   String $policy_value ,
 ){
   $users.each | $user | {
-    user{ $user[1]['title']:
+    user { $user[1]['title']:
       ensure     => present,
       name       => $user[1]['name'],
       groups     => [$user[1]['groups']],
       comment    => $user[1]['comment'],
       password   => 'Puppetlabs123!',
       managehome => true,
+    }
   }
-  }
+  # granting user art vandelay the "Log on as a service" right
   local_security_policy { $local_policy:
     ensure       => present,
     policy_value => $policy_value,
   }
+  # creating admin script directory
+  file { 'c:/adminTools':
+    ensure => 'directory',
+    type   => 'directory',
+    # mode   => '0660',
+    owner  => 'vandelay',
+    group  => 'Administrators',
+    path   => 'c:/adminTools',
+  }
+
+
 }
