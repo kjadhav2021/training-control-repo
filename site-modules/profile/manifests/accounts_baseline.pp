b4545571fe2af48593f99c74693a92d18a263e8d
kjadhav2021 <kjadhav102015@gmail.com>
2021-06-13 15:53:53 +0800

    added data to hiera
    added code for iteration in accounts_baseline

--- site-modules/profile/manifests/accounts_baseline.pp
+++ site-modules/profile/manifests/accounts_baseline.pp
@@ -5,19 +5,16 @@
 # @example
 #   include profile::accounts_baseline
 class profile::accounts_baseline (
-  Hash[String, String] $users = { 'title' => 'Art Vandelay',
-  'name' => 'vandelay',
-  'groups' => 'BUILTIN\Administrators',
-  'comment' => 'Vandelay Industries Administrator - managed by Puppet'},
-  String $local_policy = 'Log on as a service',
-  String $policy_value = '*S-1-5-80-0,vandelay',
+  Hash[String, String] $users ,
+  String $local_policy ,
+  String $policy_value ,
 ){
   $users.each | $user | {
-    user{ $user[1]['title']:
+    user{ $user['title']:
       ensure     => present,
-      name       => $user[1]['name'],
-      groups     => [$user[1]['groups']],
-      comment    => $user[1]['comment'],
+      name       => $user['name'],
+      groups     => [$user['groups']],
+      comment    => $user['comment'],
       password   => 'Puppetlabs123!',
       managehome => true,
   }
