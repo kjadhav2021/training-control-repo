c9893e6890b8a09e9a2a80f450832f327a6dad14
kjadhav2021 <kjadhav102015@gmail.com>
2021-06-22 13:45:52 +0800

    updated tpp_archive

--- site-modules/profile/manifests/install_tpp_archive.pp
+++ site-modules/profile/manifests/install_tpp_archive.pp
@@ -12,29 +12,28 @@ class profile::install_tpp_archive (
       ensure => 'directory',
       path   => $extract_path,
     }
-    # $archivesettings.each | $k,$d | {
-    #   if $d['extract'] {
-    #     archive { $d['title']:
-    #       ensure        => present,
-    #       path          => $extract_path,
-    #       extract       => $d['extract'],
-    #       extract_path  => $extract_path,
-    #       source        => $d['source'],
-    #       checksum      => $d['sha_checksum'],
-    #       checksum_type => 'sha256',
-    #       creates       => $extract_path,
-    #       require       => File[$extract_path],
-    #     }
-    #   }
-    #   # else {
-    #   #   archive { $d['title']:
-    #   #     path          => $extract_path,
-    #   #     source        => $d['source'],
-    #   #     checksum      => $d['sha_checksum'],
-    #   #     checksum_type => 'sha256',
-    #   #     provider      => 'windows',
-    #   #     require       => Archive[$d['dependent_class']],
-    #   #   }
-    #   # }
-    # }
+    $archivesettings.each | $k,$d | {
+      if $d['extract'] {
+        archive { $d['title']:
+          ensure        => present,
+          path          => $extract_path,
+          extract       => $d['extract'],
+          extract_path  => $extract_path,
+          source        => $d['source'],
+          checksum      => $d['sha_checksum'],
+          checksum_type => 'sha256',
+          creates       => $extract_path,
+        }
+      }
+      # else {
+      #   archive { $d['title']:
+      #     path          => $extract_path,
+      #     source        => $d['source'],
+      #     checksum      => $d['sha_checksum'],
+      #     checksum_type => 'sha256',
+      #     provider      => 'windows',
+      #     require       => Archive[$d['dependent_class']],
+      #   }
+      # }
+    }
 }
