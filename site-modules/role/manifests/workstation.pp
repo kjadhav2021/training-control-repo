16e018e0080112e88b08605310acc4e0795c890f
kjadhav2021 <kjadhav102015@gmail.com>
2021-06-22 11:19:54 +0800

    added new role workstation
    removed base profile class from database server role

---
+++ site-modules/role/manifests/workstation.pp
@@ -0,0 +1,12 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include role::workstation
+class role::workstation {
+  include profile::accounts_baseline
+  include profile::generic_settings
+  # include profile::install_tpp_archive
+  # include profile::install_tpp_chocolatey
+}
