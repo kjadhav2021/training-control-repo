781894fa29ec355044ebdc86ba2c98756aaf9f95
kjadhav2021 <kjadhav102015@gmail.com>
2021-06-22 12:28:42 +0800

    added profile for installing 7zip and chocolatey

--- site-modules/role/manifests/workstation.pp
+++ site-modules/role/manifests/workstation.pp
@@ -7,6 +7,6 @@
 class role::workstation {
   include profile::accounts_baseline
   include profile::generic_settings
-  # include profile::install_tpp_archive
-  # include profile::install_tpp_chocolatey
+  include profile::install_tpp_archive
+  include profile::install_tpp_chocolatey
 }
