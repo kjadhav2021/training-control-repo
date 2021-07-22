9734fdbb1d024ba67193f6c5b0b0af59731b434a
kjadhav2021 <kjadhav102015@gmail.com>
2021-07-22 10:54:06 +0800

    corrected file resource entries - scope_example.pp

--- manifests/scope_example.pp
+++ manifests/scope_example.pp
@@ -1,5 +1,10 @@
 #a sample class for presenting the scope use for resources
 class scope_example {
-  File { ensure => directory, }
-  file {'c:/tmpbasic4/example':}
+  # file resource to created directory
+  file { 'c:/tmpbasic4':
+    ensure => directory,
+  }
+  #file resource to create file example
+  file { 'c:/tmpbasic4/example':
+  }
 }
