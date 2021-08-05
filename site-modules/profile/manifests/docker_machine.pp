fb5187b754fd9cdddc506257c89ed1d9dbfb74a9
Kunal Jadhav <77015912+kjadhav2021@users.noreply.github.com>
2021-08-05 09:40:53 +0800

    updated docker_machine.pp file

--- site-modules/profile/manifests/docker_machine.pp
+++ site-modules/profile/manifests/docker_machine.pp
@@ -5,11 +5,5 @@
 # @example
 #   include profile::docker_machine
 class profile::docker_machine {
-  # include 'docker'
-  class { 'docker':
-    docker_ee                   => false,
-    use_upstream_package_source => false,
-    service_overrides_template  => false,
-    repo_opt                    => '',
-  }
+  include 'docker'
 }
